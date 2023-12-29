729ee3bcd987a0c2b5804668c77695b243c1a707
Syahrizal Hanif <syahrizalhanif@gmail.com>
2023-12-30 03:59:29 +0700

    ✨ CRUD categories

--- app/api/v1/categories/router.js
+++ app/api/v1/categories/router.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express();
+const { create, index, find, update, destroy } = require('./controller');
 
-router.get('/categories', (req, res) => {
-    res.status(200).json({
-        message: 'Halaman categories',
-    });
-});
+router.get('/categories', index);
+router.get('/categories/:id', find);
+router.put('/categories/:id', update);
+router.delete('/categories/:id', destroy);
+router.post('/categories', create);
 
 module.exports = router;
