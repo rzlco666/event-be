10ae7b30998a32bd5c5e613a9b93a3b0cf5a883d
Syahrizal Hanif <syahrizalhanif@gmail.com>
2023-12-30 02:07:32 +0700

    ♻️ Change pattern

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const app = express();
 
-var app = express();
+// router
+const categoriesRouter = require('./app/api/v1/categories/router');
+
+const v1 = '/api/v1/cms';
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -14,7 +16,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.get('/', (req, res) => {
+    res.status(200).json({
+        message: 'Welcome to API',
+    });
+});
+
+app.use(v1, categoriesRouter);
 
 module.exports = app;
